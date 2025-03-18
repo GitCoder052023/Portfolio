d6da58476346702052b04277aabfce22bc544f27
GitCoder0502023 <K.alam93899@gmail.com>
2025-03-18 15:46:24 +0530

    Remove start.js script for running server and frontend processes

--- start.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process");
-
-const serverProcess = exec("node Server/server.js");
-serverProcess.stdout.on("data", (data) => console.log(`Server: ${data}`));
-serverProcess.stderr.on("data", (data) => console.error(`Server Error: ${data}`));
-
-const frontendProcess = exec("node src/app.js");
-frontendProcess.stdout.on("data", (data) => console.log(`Frontend: ${data}`));
-frontendProcess.stderr.on("data", (data) => console.error(`Frontend Error: ${data}`));
